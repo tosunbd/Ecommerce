4eaf028a92412088f5a8532c446d1fb5c07ea029
tosunbd <tosunbd@gmail.com>
2024-05-26 21:11:13 +0600

    vite config and App.jsx updated

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -1,7 +1,7 @@
-import { useState } from 'react';
-import './App.css';
-import Router from './routers/Router';
-import publicRoutes from './routers/routes/publicRoutes';
+import { useState } from "react";
+import "./App.css";
+import Router from "./routers/Router";
+import publicRoutes from "./routers/routes/publicRoutes";
 
 function App() {
   const [allRoutes, setAllRoutes] = useState([...publicRoutes]);
