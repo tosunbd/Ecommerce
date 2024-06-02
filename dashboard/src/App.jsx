03b6bf9912764ced54f1f44a78b2d0c0df2f35d2
tosun <tosunbd@gmail.com>
2024-06-02 17:58:21 +0600

    updated

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -1,12 +1,18 @@
+<<<<<<< HEAD
+import { useState } from 'react';
+import './App.css';
+import Router from './router/Router';
+import publicRoutes from './router/routes/publicRoutes';
+=======
 import { useState } from "react";
 import "./App.css";
 import Router from "./routers/Router";
 import publicRoutes from "./routers/routes/publicRoutes";
+>>>>>>> 4eaf028a92412088f5a8532c446d1fb5c07ea029
 
 function App() {
   const [allRoutes, setAllRoutes] = useState([...publicRoutes]);
-  console.log(allRoutes);
-
+  // console.log(allRoutes);
   return <Router allRoutes={allRoutes} />;
 }
 
--- dashboard/src/router/Router.js
+++ dashboard/src/router/Router.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useRoutes } from 'react-router-dom';
 
 const Router = ({ allRoutes }) => {
     const routes = useRoutes([...allRoutes]);
