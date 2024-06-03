e6b3b97bedbc0e4bca2191036d0d948012fe9d8f
tosun <tosunbd@gmail.com>
2024-06-03 16:41:11 +0600

    AdminLogin Added with logo.png

--- dashboard/src/router/routes/publicRoutes.js
+++ dashboard/src/router/routes/publicRoutes.js
@@ -1,6 +1,8 @@
 import { lazy } from "react";
+
 const Login = lazy(() => import("../../views/auth/Login"));
 const Register = lazy(() => import("../../views/auth/Register"));
+const AdminLogin = lazy(() => import("../../views/auth/AdminLogin"));
 
 const publicRoutes = [
   {
@@ -10,6 +12,10 @@ const publicRoutes = [
   {
     path: "/register",
     element: <Register />,
+  },
+  {
+    path: "/admin/login",
+    element: <AdminLogin />,
   },
 ];
 
---
+++ dashboard/src/views/auth/AdminLogin.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+const AdminLogin = () => {
+
+    const [state, setState] = useState({        
+        email: '',
+        password: ''
+    });
+
+    const inputHandle = (e) => {
+        setState({
+            ...state,
+            [e.target.name]: e.target.value            
+        })
+    }
+
+    const submit = (e) => {
+        e.preventDefault();
+        console.log(state);
+    }
+
+    return (
+        <div className='min-w-screen min-h-screen bg-[#cdcae9] flex justify-center items-center'>            
+            <div className='w-[350px] text-[#ffffff] p-2'>
+                <div className='bg-[#6f68d1] p-4 rounded-md'>
+                   
+                    <div className='h-[70px] flex justify-center items-center'>
+                        <div className='w-[180px] h-[50px]'>
+                            <img className='w-full h-full' src="http://localhost:5173/images/logo.png" alt="image" />
+                        </div>
+                    </div>
+
+                    <form onSubmit={submit}>                    
+
+                        <div className='flex flex-col w-full gap-1 mb-3'>
+                            <label htmlFor="email">Email</label>
+                            <input onChange={inputHandle} value={state.email} className=' px-3 py-2 outline-none border border-slate-400 bg-transparent rounded-md' type="email" name='email' id='email' placeholder='Email' required />
+                        </div>
+
+                         <div className='flex flex-col w-full gap-1 mb-3'>
+                            <label htmlFor="name">Password</label>
+                            <input onChange={inputHandle} value={state.password} className=' px-3 py-2 outline-none border border-slate-400 bg-transparent rounded-md' type="password" name='password' id='password' placeholder='Password' required />
+                        </div>
+
+
+                        <button className='bg-slate-800 w-full hover:shadow-blue-300/hover:shadow-lg text-white rounded-md px-7 py-2 mb-3'>Login</button>
+                  
+                    </form>
+
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default AdminLogin;
