6ed85690bdfbec42ab135e224bfae11c78366ac3
tosunbd <tosunbd@gmail.com>
2024-07-13 20:49:38 +0600

    Add Product updated

--- dashboard/src/views/seller/AddProduct.jsx
+++ dashboard/src/views/seller/AddProduct.jsx
@@ -1,8 +1,18 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { IoMdClose } from 'react-icons/io';
+import Category from './../admin/Category';
 
 const AddProduct = () => {
+    const categories = [
+        { id: 1, name: 'Sports' },
+        { id: 2, name: 'Tshirt' },
+        { id: 3, name: 'Mobile' },
+        { id: 4, name: 'Computer' },
+        { id: 5, name: 'Watch' },
+        { id: 6, name: 'Pant' }
+    ];
+
     const [state, setState] = useState({
         name: '',
         description: '',
@@ -13,11 +23,28 @@ const AddProduct = () => {
         category: ''
     });
 
+    const [cateShow, setCateShow] = useState(false);
+    const [category, setCategory] = useState('');
+    const [searchValue, setSearchValue] = useState('');
+    const [allCategory, setAllCategory] = useState(categories);
+
     const inputHandle = (e) => {
         setState({
             ...state,
             [e.target.name]: e.target.value
         });
+    };
+
+    const categorySearch = (e) => {
+        const value = e.target.value;
+        setSearchValue(value);
+        
+        if (value) {
+            const srcValue = categories.filter(c => c.name.toLowerCase().includes(value.toLowerCase()));
+            setAllCategory(srcValue);
+        } else {
+            setAllCategory(categories);
+        }
     };
 
     return (
@@ -31,7 +58,7 @@ const AddProduct = () => {
                     <form action="">
                         <div className='grid grid-cols-2 gap-4 mb-3 text-[#d0d2d6]'>
                             <div className='flex flex-col gap-1'>
-                                <label htmlFor="name">Product Name</label>
+                                <label className='text-left' htmlFor="name">Product Name</label>
                                 <input
                                     className='px-4 py-2 focus:border-indigo-500 outline-none bg-[#6a5fdf] border border-slate-700 rounded-md text-[#d0d2d6]'
                                     onChange={inputHandle}
@@ -43,7 +70,7 @@ const AddProduct = () => {
                                 />
                             </div>
                             <div className='flex flex-col gap-1'>
-                                <label htmlFor="brand">Brand Name</label>
+                                <label className='text-left' htmlFor="brand">Brand Name</label>
                                 <input
                                     className='px-4 py-2 focus:border-indigo-500 outline-none bg-[#6a5fdf] border border-slate-700 rounded-md text-[#d0d2d6]'
                                     onChange={inputHandle}
@@ -55,22 +82,52 @@ const AddProduct = () => {
                                 />
                             </div>
                         </div>
+
                         <div className='grid grid-cols-2 gap-4 mb-3 text-[#d0d2d6]'>
-                            <div className='flex flex-col gap-1'>
-                                <label htmlFor="category">Category</label>
-                                <select
-                                    className='px-4 py-2 focus:border-indigo-500 outline-none bg-[#6a5fdf] border border-slate-700 rounded-md text-[#d0d2d6]'
-                                    onChange={inputHandle}
-                                    value={state.category}
+                            <div className='flex flex-col gap-1 relative'>
+                                <label className='text-left' htmlFor="category">Category</label>
+                                <input
+                                    type="text"
+                                    onClick={() => setCateShow(!cateShow)}
+                                    className='px-4 py-2 focus:border-indigo-500 outline-none bg-[#6a5fdf] 
+                                    border border-slate-700 rounded-md text-[#d0d2d6]'
+                                    onChange={categorySearch}
+                                    value={category}
                                     name='category'
                                     id='category'
-                                >
-                                    <option value="">--select category--</option>
-                                    {/* Add category options here */}
-                                </select>
+                                />
+
+                                <div className={`absolute top-[101%] bg-[#475569] w-full transition-all ${cateShow ? 'scale-100' : 'scale-0'}`}>
+                                    <div className='w-full px-4 py-2'>
+                                        <input
+                                            type="text"
+                                            onChange={categorySearch}
+                                            value={searchValue}
+                                            className='px-3 py-1 w-full focus:border-indigo-500 
+                                            outline-none bg-[#6a5fdf] border border-slate-700
+                                            rounded-md text-[#d0d2d6]'
+                                            placeholder='Search'
+                                        />
+                                    </div>
+                                    <div className='flex flex-col justify-start items-start h-[200px] overflow-y-scroll mt-2 text-left'>
+                                        {allCategory.map((c, i) => (
+                                            <span
+                                                key={i}
+                                                onClick={() => {
+                                                    setCateShow(false);
+                                                    setCategory(c.name);
+                                                    setSearchValue('');
+                                                    setAllCategory(categories);
+                                                }}
+                                                className='cursor-pointer p-1 w-full hover:bg-indigo-500'
+                                            >{c.name}</span>
+                                        ))}
+                                    </div>
+                                </div>
+
                             </div>
                             <div className='flex flex-col gap-1'>
-                                <label htmlFor="stock">Product Stock</label>
+                                <label className='text-left' htmlFor="stock">Product Stock</label>
                                 <input
                                     className='px-4 py-2 focus:border-indigo-500 outline-none bg-[#6a5fdf] border border-slate-700 rounded-md text-[#d0d2d6]'
                                     onChange={inputHandle}
@@ -82,9 +139,10 @@ const AddProduct = () => {
                                 />
                             </div>
                         </div>
+
                         <div className='grid grid-cols-2 gap-4 mb-3 text-[#d0d2d6]'>
                             <div className='flex flex-col gap-1'>
-                                <label htmlFor="price">Price</label>
+                                <label className='text-left' htmlFor="price">Price</label>
                                 <input
                                     className='px-4 py-2 focus:border-indigo-500 outline-none bg-[#6a5fdf] border border-slate-700 rounded-md text-[#d0d2d6]'
                                     onChange={inputHandle}
@@ -96,7 +154,7 @@ const AddProduct = () => {
                                 />
                             </div>
                             <div className='flex flex-col gap-1'>
-                                <label htmlFor="discount">Discount</label>
+                                <label className='text-left' htmlFor="discount">Discount</label>
                                 <input
                                     className='px-4 py-2 focus:border-indigo-500 outline-none bg-[#6a5fdf] border border-slate-700 rounded-md text-[#d0d2d6]'
                                     onChange={inputHandle}
@@ -108,6 +166,7 @@ const AddProduct = () => {
                                 />
                             </div>
                         </div>
+
                         <div className='flex flex-col mb-3 gap-4 text-[#d0d2d6]'>
                             <div className='flex flex-col gap-1'>
                                 <label htmlFor="description">Description</label>
@@ -121,6 +180,7 @@ const AddProduct = () => {
                                 />
                             </div>
                         </div>
+
                         <div className='flex gap-4 mb-3'>
                             <div className='relative w-1/2'>
                                 <div className='border p-2 rounded-md'>
@@ -137,7 +197,14 @@ const AddProduct = () => {
                                 </label>
                             </div>
                         </div>
-                        <button type="submit" className='bg-green-500 hover:shadow-green-500/50 hover:shadow-lg text-white rounded-sm px-7 py-2'>Add Product</button>
+                        <div className='flex justify-start'>
+                            <button
+                                type="submit"
+                                className='bg-green-500 hover:shadow-green-500/50
+                                hover:shadow-lg text-white rounded-sm px-7 py-2 self-start'>
+                                Add Product
+                            </button>
+                        </div>
                     </form>
                 </div>
             </div>
