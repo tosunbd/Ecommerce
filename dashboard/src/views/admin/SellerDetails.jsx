8641e62f6baa3897b42d1ef3a67daa64d3e6fda8
tosun <tosunbd@gmail.com>
2024-11-18 17:07:17 +0600

    Seller Details And Status Manage completed

--- dashboard/src/views/admin/SellerDetails.jsx
+++ dashboard/src/views/admin/SellerDetails.jsx
@@ -61,8 +61,7 @@ const SellerDetails = () => {
                     <div className='w-3/12 flex justify-center items-center py-3'>
                         <div>
                             { 
-                               seller?.image ? <img className='w-full h-[230px]' src="http://localhost:5173/
-                               images/demo.jpg" alt="" />
+                               seller?.image ? <img className='w-full h-[230px]' src={seller.image} alt="" />
                                 :
                                 <span>No Image Found</span>
                             }                            
