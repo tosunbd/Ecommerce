ff2274c45e8356a7cd71657871220d3347aa182d
tosun <tosunbd@gmail.com>
2024-11-20 13:45:30 +0600

    frontend updated

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from '../components/Header';
+
+const Home = () => {
+    return (
+        <div className='w-full'>
+            <div className='header-top'>
+                <div>
+                    <ul>
+                        <li></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
