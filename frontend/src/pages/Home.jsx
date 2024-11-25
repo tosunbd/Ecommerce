a98d0f9a6a5e14ceb0e1d91531e401c1d49a1629
tosun <tosunbd@gmail.com>
2024-11-25 12:31:19 +0600

    updated fronted

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,13 +4,7 @@ import Header from '../components/Header';
 const Home = () => {
     return (
         <div className='w-full'>
-            <div className='header-top'>
-                <div>
-                    <ul>
-                        <li></li>
-                    </ul>
-                </div>
-            </div>
+            <Header />
         </div>
     );
 };
